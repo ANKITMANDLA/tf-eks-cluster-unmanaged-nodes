f5f06d2e77b680b6012bb5fca38b0bbd65ae9ece
ANKITMANDLA <36769449+ANKITMANDLA@users.noreply.github.com>
2023-05-05 15:35:09 -0400

    Update main.tf

--- modules/infrastructure-group/eks-private-dev/main.tf
+++ modules/infrastructure-group/eks-private-dev/main.tf
@@ -1,20 +1,20 @@
 locals {
-  vpc_id = "vpc-01072b8f491be8f88"
-  subnet_ids = ["subnet-06ec73b83b12ab447","subnet-006f499fab8a7e27e","subnet-0a447f36e719de045"]
+  vpc_id = "vpc-XXXXX"
+  subnet_ids = ["subnet-X","subnet-XX","subnet-XXX"]
   eks_name = "${var.custom_domain}-eks"
   eks_version = "1.18"
   eks_standard_worker_group_name = "${local.eks_name}-standard-worker-group"
   eks_standard_worker_group_disk_size = 25
   eks_standard_worker_group_type = "m5a.xlarge"
   eks_standard_worker_node_count = 3
-  eks_role_arn = "arn:aws:iam::893130235090:role/smp-eks-cluster" #iam role for eks control plane
-  additional_control_plane_sg_ids = ["sg-087fff5e699e74f4e"]
-  additional_worker_nodes_sg_ids = ["sg-087fff5e699e74f4e"]
-  eks_worker_node_ami_image_id = "ami-0c29dd87e87fb4dfd"
-  eks_worker_node_instance_role = "arn:aws:iam::893130235090:role/smp-eks-node-instance"
-  eks_worker_node_ssh_key_name = "vm-ssh-qa-key"
+  eks_role_arn = "arn:aws:iam::XX:role/example-eks-cluster" #iam role for eks control plane
+  additional_control_plane_sg_ids = ["sg-XX"]
+  additional_worker_nodes_sg_ids = ["sg-XX"]
+  eks_worker_node_ami_image_id = "ami-XX"
+  eks_worker_node_instance_role = "arn:aws:iam::XX:role/example-eks-node-instance"
+  eks_worker_node_ssh_key_name = "vm-ssh-key"
   cluster_access_role = "arn:aws:iam::893130235090:role/smp-eks-cluster"
-  kube_config_store_bucket = "hansencx-prov-vm-qa-kube-config"
+  kube_config_store_bucket = "example-prov-vm-kube-config"
 }
 
 module "eks_security" {
@@ -25,7 +25,7 @@ module "eks_security" {
   group_name       = var.custom_domain
 }
 
-module "hansen_security" {
+module "example_security" {
   source = "../../../modules/common-sg"
 
   eks_cluster_name = local.eks_name
@@ -38,7 +38,7 @@ module "eks_control_plane" {
   source = "../../../modules/k8s/eks-control-plane"
   
   group_name       = var.custom_domain
-  control_plane_sg = concat([module.eks_security.control_plane_sg_id],[module.hansen_security.hansen-common-sg-id], local.additional_control_plane_sg_ids)
+  control_plane_sg = concat([module.eks_security.control_plane_sg_id],[module.example_security.example-common-sg-id], local.additional_control_plane_sg_ids)
   name             = local.eks_name
   subnet_ids       = local.subnet_ids
   eks_version      = local.eks_version
@@ -56,7 +56,7 @@ module "worker_node_group" {
   instance_type       = local.eks_standard_worker_group_type
   subnet_ids          = local.subnet_ids
   node_count          = local.eks_standard_worker_node_count
-  worker_sg           = concat([module.eks_security.worker_node_sg_id],[module.hansen_security.hansen-common-sg-id], local.additional_control_plane_sg_ids) 
+  worker_sg           = concat([module.eks_security.worker_node_sg_id],[module.example_security.example-common-sg-id], local.additional_control_plane_sg_ids) 
   ami_image_id        = local.eks_worker_node_ami_image_id
   node_instance_role  = local.eks_worker_node_instance_role
   ssh_key_name        = local.eks_worker_node_ssh_key_name
@@ -71,17 +71,4 @@ module "eks_kubeconfig" {
   kube_config_store_bucket = local.kube_config_store_bucket
 }
 
-module "smp_k8s_prep" {
-  source  = "../../../modules/smp/smp-k8s-prep"
 
-  k8s_config_raw            = module.eks_kubeconfig.kube_config_raw
-  cluster_access_role       = local.cluster_access_role
-  active                    = var.active
-}
-
-module "smp_startup" {
-  source = "../../../modules/smp/smp-startup"
-  
-  k8s_config_raw = module.eks_kubeconfig.kube_config_raw
-  active         = var.active
-}
